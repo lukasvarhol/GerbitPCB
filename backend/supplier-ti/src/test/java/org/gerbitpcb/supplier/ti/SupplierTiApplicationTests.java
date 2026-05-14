39f7a16ea3588aef515ca2c95cbd537e3e67b338
ronscio <lorenzo.fabbri@student.kuleuven.be>
2026-05-14 23:14:23 +0200

    0auth implementation

--- backend/supplier-ti/src/test/java/org/gerbitpcb/supplier/ti/SupplierTiApplicationTests.java
+++ backend/supplier-ti/src/test/java/org/gerbitpcb/supplier/ti/SupplierTiApplicationTests.java
@@ -2,9 +2,14 @@ package org.gerbitpcb.supplier.ti;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 class SupplierTiApplicationTests {
+
+    @MockBean
+    JwtDecoder jwtDecoder;
 
     @Test
     void contextLoads() {
