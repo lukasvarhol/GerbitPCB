1994ccd8c94ce2baeaf351bfd268a2e3eb58369e
Stijn <stijngeens07@gmail.com>
2026-06-11 11:00:09 +0200

    [fix::broker]: StockUpdateRequest: allow stock update request to push an empty zero quantity to the broker's copy of the suppliers' tables (wrong annotation was used: @Positive vs @PositiveOrZero)

--- backend/broker-service/src/main/java/org/gerbitpcb/broker/dto/StockUpdateRequest.java
+++ backend/broker-service/src/main/java/org/gerbitpcb/broker/dto/StockUpdateRequest.java
@@ -1,8 +1,8 @@
 package org.gerbitpcb.broker.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
-public record StockUpdateRequest(@NotBlank String sku, @NotBlank String supplier, @Positive int availableStock){
+public record StockUpdateRequest(@NotBlank String sku, @NotBlank String supplier, @PositiveOrZero int availableStock){
 }
 			       
