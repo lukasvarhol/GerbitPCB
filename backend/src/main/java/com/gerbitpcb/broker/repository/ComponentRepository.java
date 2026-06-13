09e445511d90038518b072be04b830fddfbcb745
Lukas Varhol <luka.varhol@gmail.com>
2026-06-13 19:01:38 +0200

    [cleanup]: tidy repo structure

--- backend/src/main/java/com/gerbitpcb/broker/repository/ComponentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gerbitpcb.broker.repository;
-
-import com.gerbitpcb.broker.model.Component;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComponentRepository extends JpaRepository<Component, String> {}
--- backend/src/main/java/com/gerbitpcb/broker/repository/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gerbitpcb.broker.repositroy;
-
-import com.gerbit.broker.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, String> {}
